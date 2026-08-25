fa2d4aa9d86d3d1b0d033e34b9ec69a7c0d8df49
Prakash <prakash131204@gmail.com>
2026-08-26 04:52:09 +0800

    Level-3: Allow completed tasks to be unmarked

--- src/main/java/Orbit.java
+++ src/main/java/Orbit.java
@@ -45,6 +45,11 @@ public class Orbit {
                 taskStatuses[taskIndex] = true;
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println("  [X] " + tasks[taskIndex]);
+            } else if (input.startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(input.substring(7)) - 1;
+                taskStatuses[taskIndex] = false;
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  [ ] " + tasks[taskIndex]);
             } else {
                 tasks[taskCount] = input;
                 taskCount++;
